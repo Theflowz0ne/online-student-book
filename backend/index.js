f689bab4e260bd451b1803758a9c30c17b5ba7f6
Ivan Boyanov <Theflowzon3@gmail.com>
2022-12-11 14:48:21 +0200

    structure config

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,38 @@
 import express from 'express';
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import cors from 'cors';
+
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(cors());
 const prisma = new PrismaClient();
 
 app.get('/', async (req, res) => {
-    const allAdmins = await prisma.admin.findMany()
-    res.send(allAdmins);
+	const allAdmins = await prisma.admin.findMany()
+	res.send(allAdmins);
 });
 
 app.get('/test', async (req, res) => {
-    await prisma.admin.create({
-      data: {
-        email: req.query.email,
-        password: req.query.password,
-        id: Number(req.query.id)
-      },
-    });
-    res.send();
+	await prisma.admin.create({
+	data: {
+		email: req.query.email,
+		password: req.query.password,
+		id: Number(req.query.id)
+	},
+	});
+	res.send();
 });
 
-app.get("/edu", async (req, res) => {
-  await prisma.educationForm.create({
-    data: {
-      educationForm : "Redovno"
-    },
-  });
-  res.send();
+app.get('/edu', async (req, res) => {
+	await prisma.educationForm.create({
+		data: {
+		educationForm : 'Redovno'
+		},
+	});
+	res.send();
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
