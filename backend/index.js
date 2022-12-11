04cede2647c471f559abcd2933300e806d0c26da
Ivan Boyanov <Theflowzon3@gmail.com>
2022-12-11 15:31:00 +0200

    alignment

--- backend/index.js
+++ backend/index.js
@@ -34,35 +34,35 @@ app.get('/edu', async (req, res) => {
 });
 
 app.get("/discipline/get", async (req, res) => {
-  try {
-	const disciplines = await prisma.discipline.findMany()
-    res.status(200).send(disciplines);
-  } catch (err) {
-	console.log(err);
-	res.status(500).send("Something went wrong");
-  }
+  	try {
+		const disciplines = await prisma.discipline.findMany()
+		res.status(200).send(disciplines);
+  	} catch (err) {
+		console.log(err);
+		res.status(500).send("Something went wrong");
+  	}
 });
 
 app.get("/discipline/get/:id", async (req, res) => {
-  try {
-    const discipline = await prisma.discipline.findUnique({
-		where: {
-			id: Number(req.params.id)
-		}
-	});
-    res.status(200).send(discipline);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something went wrong");
-  }
+  	try {
+    	const discipline = await prisma.discipline.findUnique({
+			where: {
+				id: Number(req.params.id)
+			}
+		});
+    	res.status(200).send(discipline);
+  	} catch (err) {
+    	console.log(err);
+    	res.status(500).send("Something went wrong");
+  	}
 });
 
 app.post('/discipline/create', async (req, res) => {
 	try {
 		const discipline = await prisma.discipline.create({
-		data: {
-			discipline: req.body.name,
-		},
+			data: {
+				discipline: req.body.name,
+			},
 		});
 		res.status(200).send(discipline);
 	} catch (err) {
@@ -89,17 +89,17 @@ app.put('/discipline/update/:id', async (req, res) => {
 });
 
 app.delete("/discipline/delete/:id", async (req, res) => {
-  try {
-    await prisma.discipline.delete({
-      where: {
-        id: Number(req.params.id),
-      },
-    });
-    res.status(200).send();
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something went wrong");
-  }
+	try {
+		await prisma.discipline.delete({
+			where: {
+			id: Number(req.params.id),
+			},
+		});
+		res.status(200).send();
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("Something went wrong");
+	}
 });
 
 app.listen(port, () => {
