17c43dd4b60e9944a09e5b190f065a49ffd5479d
Ivan Boyanov <Theflowzon3@gmail.com>
2022-12-11 10:38:35 +0200

    Install Prisma Client

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,35 @@
 import express from 'express';
+import { PrismaClient } from "@prisma/client";
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get('/', async (req, res) => {
+    const allAdmins = await prisma.admin.findMany()
+    res.send(allAdmins);
 })
 
-app.get('/test', (req, res) => {
+app.get('/test', async (req, res) => {
+    await prisma.admin.create({
+      data: {
+        email: req.query.email,
+        password: req.query.password,
+        id: Number(req.query.id)
+      },
+    });
     res.send();
 })
+
+app.get("/edu", async (req, res) => {
+  await prisma.educationForm.create({
+    data: {
+      educationForm : "Redovno"
+    },
+  });
+  res.send();
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
+
