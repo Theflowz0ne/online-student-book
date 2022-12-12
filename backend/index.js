68bc240f50031c17516e4f85bb87959a64b23bc8
Ivan Boyanov <Theflowzon3@gmail.com>
2022-12-12 20:12:25 +0200

    index import

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import disciplineRoutes from './src/routes/disclipline.js';
+import gradeRoutes from "./src/routes/grade.js";
+import studentRoutes from "./src/routes/student.js";
 
 const app = express();
 const port = 3000;
@@ -8,6 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(disciplineRoutes);
+app.use(gradeRoutes);
+app.use(studentRoutes);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
